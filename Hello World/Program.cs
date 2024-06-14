9cc8386f6466056399d82b5cdb2d0d6775435f6a
Hopush <nady555malaya@gmail.com>
2024-06-14 10:37:58 +0300

    Добавьте файлы проекта.

---
+++ Hello World/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Hello_World;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите свое имя:");
+        string name = Console.ReadLine();
+        DateTime currentDate = DateTime.Now;
+        Console.WriteLine($"\n Привет {name}, сейчас {currentDate:d} at {currentDate:t}!");
+        Console.Write($"\n Нажмите любую клавишу для завершения...");
+        Console.ReadKey(true);
+    }
+}
